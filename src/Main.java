171555244c60850fbf5aaeac2f18ac85146fc758
Admin <k.i.r.s@bk.ru>
2022-10-25 12:14:47 +0300

    Задание 1

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        //Задание 1
+        int age = 18;
+        if (age >= 18) {
+            System.out.println("Поздравляю - Вы совершеннолетний человек!!!");
+        }
+        if (age < 18) {
+            System.out.println("Возраст совершеннолетия ещё не наступил. У Вас всё впереди!");
+        }
+    }
+}
+
+
+
